36f2b0978e05cd43e03ae3a238710892eea26573
samkit4q <a.aderoba@alustudent.com>
2024-11-08 13:48:40 -0800

    script updated

--- backend/SamEducation/urls.py
+++ backend/SamEducation/urls.py
@@ -1,9 +1,9 @@
-# SamEducation/urls.py
+# backend/sam_education/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),  # Includes URLs from the users app
+    path('users/', include('users.urls')),  # Include the user-related URLs
 ]
 
